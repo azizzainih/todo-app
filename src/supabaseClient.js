4368e3ef457f1fa473e83cf850e01bb374c7660b
Your Name <azizzainih@gmail.com>
2024-12-02 17:35:04 +0700

    Fix the supabase key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseUrl = 'https://pddaheejvijsusnibcmn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkZGFoZWVqdmlqc3VzbmliY21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1ODkyNzcsImV4cCI6MjA0ODE2NTI3N30.D1l280cmh8X2WdlVIfxYBov7DReALSA1LbBvdqh3vkQ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
